99445a7e19bfd07bc9324f1988b57106ccdb235e
liuweijian <279423484@qq.com>
2025-06-24 22:04:59 +0800

    添加页面缓存

---
+++ my_demo/lib/pages/keepalive/keepalive.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+/* 
+AutomaticKeepAliveClientMixin 可以快速的实现页面缓存功能，但是通过混入的方式实现不是很优
+雅， 所以我们有必要对AutomaticKeepAliveClientMixin 混入进行封装
+ */
+class KeepAliveWrapper extends StatefulWidget {
+  const KeepAliveWrapper({
+    super.key,
+    required this.child,
+    required this.keepAlive,
+  });
+
+  final Widget? child;
+  final bool keepAlive;
+
+  @override
+  State<KeepAliveWrapper> createState() => _KeepaliveState();
+}
+
+class _KeepaliveState extends State<KeepAliveWrapper>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    return widget.child!;
+  }
+
+  @override
+  // TODO: implement wantKeepAlive
+  bool get wantKeepAlive => true;
+
+  @override
+  void didUpdateWidget(covariant KeepAliveWrapper oldWidget) {
+    if (oldWidget.keepAlive != widget.keepAlive) {
+      // keepAlive 状态需要更新，实现在 AutomaticKeepAliveClientMixin 中
+      updateKeepAlive();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+}
